ce33b398a45c934b89be4b7428fe45b2ec9fb92f
victorbalan <victor.balan93@gmail.com>
2019-01-03 18:43:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-window.location.host.indexOf('weltimobiliare')
+javascript:(function(){
+  let scriptSrc = '';
+  const wloc = window.location.host; 
+  if(wloc.indexOf('weltimobiliare') !== -1) {
+    scriptSrc = 'https://cdn.jsdelivr.net/gh/victorbalan/imoscripts@0.6/welt.js';
+  } else {
+    alert('Site ul ' + wloc + ' nu este suportat inca'); 
+  }
+  document.body.appendChild(document.createElement('script')).src=scriptSrc;
+})();
