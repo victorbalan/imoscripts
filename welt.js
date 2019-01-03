93677de78124b1ce288d20ab3b962bf6bd57c88d
victorbalan <victor.balan93@gmail.com>
2019-01-03 18:03:31 +0200

    Format

--- welt.js
+++ welt.js
@@ -14,13 +14,32 @@ javascript:(function(){
     li.append(price); 
     return li; 
   };  
-  const getValueFromList =  (l, type, valueIndex) => { for(let i = 0; i < l.length; i++) { let elem = l[i]; if(elem.children[0].innerText == type) { return elem.childNodes[valueIndex]; } } };  
-  const getSup = (l) => { let value = getValueFromList(l, 'Suprafata', 1).wholeText.replace(/\D+/g, ''); return Number.parseFloat(value); }; 
-  const getPrice = (l) => { let value = getValueFromList(l, 'Pret vanzare', 2).innerText.replace(/\D+/g, ''); return Number.parseFloat(value); }; 
-  const getMP = (l) => { let sup = getSup(l); let price = getPrice(l); return Math.round(price/sup); }; 
+  const getValueFromList =  (l, type, valueIndex) => {
+    for(let i = 0; i < l.length; i++) {
+      let elem = l[i]; 
+      if(elem.children[0].innerText == type) {
+        return elem.childNodes[valueIndex]; 
+      }
+    } 
+  };
+  const getSup = (l) => { 
+    let value = getValueFromList(l, 'Suprafata', 1).wholeText.replace(/\D+/g, ''); 
+    return Number.parseFloat(value); 
+  }; 
+  const getPrice = (l) => {
+    let value = getValueFromList(l, 'Pret vanzare', 2).innerText.replace(/\D+/g, '');
+    return Number.parseFloat(value);
+  }; 
+  const getMP = (l) => { 
+    let sup = getSup(l); 
+    let price = getPrice(l); 
+    return Math.round(price/sup); 
+  }; 
+  
   let details = document.getElementsByClassName('.details');
   for(let i=0; i < details.length; i++) {
     let ul = details[i];
-    let children = ul.getElementsByTagName('li');  ul.append(createLi(getMP(children))); 
+    let children = ul.getElementsByTagName('li');  
+    ul.append(createLi(getMP(children))); 
   }
 })();
